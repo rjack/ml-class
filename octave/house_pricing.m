2734ed212a321aba3d39a713de3959e606f140e4
Giacomo Ritucci <giacomo.ritucci@email.it>
2011-10-03 12:45:58 +0200

    playing with convergence

--- octave/house_pricing.m
+++ octave/house_pricing.m
@@ -1,7 +1,7 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
-% Gradient Descent algorithm for the optimization of the Cost Function in
-% Linear Regression.
+% Gradient Descent algorithm for Cost Function optimization in Linear
+% Regression.
 %
 %
 % Lectures: II. Linear Regression With One Variable
@@ -29,7 +29,7 @@ m = 100;
 x_min = 50;
 x_max = 200;
 
-alpha = 0.000001;
+alpha = 0.00001;
 
 
 function [th0_step, th1_step] = gradient_descent_step(th0, th1, xs, ys, m, alpha)
@@ -40,31 +40,54 @@ function [th0_step, th1_step] = gradient_descent_step(th0, th1, xs, ys, m, alpha
 endfunction
 
 
-function j = cost(th0, th1, xs, ys, m)
-	sum(th0 + th1 * xs - ys)/m;
+function j = cost (th0, th1, xs, ys, m)
+	j = sum((th0 + th1 * xs - ys)^2) / (2 * m);
 endfunction
 
+
+function res = converge (dx, dy, small_enough = 0)
+	if (abs(dx) <= small_enough && abs(dy) <= small_enough)
+		res = true;
+	else
+		res = false;
+	endif
+endfunction
+
+
+function die_if_diverged (x)
+	if (isnan(x))
+		printf("Diverged!\n");
+		exit(1);
+	endif
+endfunction
+
+
 xs = x_min + rand(1, m) * (x_max - x_min);
-ys = xs + -(xs/2) + xs .* rand(1, m);
+ys = 3 * xs + -(xs/2) + xs .* rand(1, m);
 
 th0 = 0;
-th1 = 1;
+th1 = 10;
 
 figure;
 plot(xs, ys, "x");
 hold on;
 
 
-for foo = 1:200
+tries = max_tries = 1000;
+while (tries--)
 	plot(x_min:x_max, th0 + th1 * (x_min:x_max))
 	[th0_step, th1_step] = gradient_descent_step(th0, th1, xs, ys, m, alpha);
-	if (th0_step == 0 && th1_step == 0)
+	if (converge(th0_step, th1_step, alpha))
 		break;
 	else
 		th0 = th0 - th0_step;
 		th1 = th1 - th1_step;
-	endif
-endfor
 
-disp(th0);
-disp(th1);
+		die_if_diverged(th0);
+		die_if_diverged(th1);
+	endif
+endwhile
+
+printf("After %d iterations (out of %d)\n", max_tries - tries - 1, max_tries);
+printf("th0 = %g\n", th0);
+printf("th1 = %g\n", th1);
